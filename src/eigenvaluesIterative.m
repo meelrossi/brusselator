211ae9c61e41b2f0d8396b74c9b685b17cbd1233
Joaquín Suárez Bodean <jsuarezb@itba.edu.ar>
2016-09-25 16:58:33 -0300

    Givens rotation implemented

--- src/eigenvaluesIterative.m
+++ src/eigenvaluesIterative.m
@@ -1,8 +1,9 @@
 function e = eigenvaluesIterative(m, L, delta1, delta2, alpha, betha)
-	A = discreteJacobian(m, L, delta1, delta2, alpha, betha);
+	# A = discreteJacobian(m, L, delta1, delta2, alpha, betha)
+  # A = [6 5 0; 5 1 4; 0 4 3];
 	flag = true;
 	while (flag)
-		[Q,R]= qr(A);
+		[Q, R]= qrgivens(A);
 		Aaux = A;
 		A = R * Q;
 		Error = Aaux - A;
@@ -10,7 +11,7 @@ function e = eigenvaluesIterative(m, L, delta1, delta2, alpha, betha)
 		for i=1 : 1 : size(A)[2];
 			errors += abs(Error(i,i));
 		end;
-		if (errors / size(A)(1) < 2e-1)
+		if (errors / size(A)(1) < 1e-20)
 			flag = false;
 		end;
 		errors / size(A)(1)
